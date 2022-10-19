9b5e6f341a0b7aac6faea58ae986b0a75bbb4725
Svetlana <iilsojar@gmail.com>
2022-10-19 09:34:23 +0200

    по условям задачи найшли минимальное и максимальное из двух чисел

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,3 +4,19 @@
 //a = -9 b = -3 -> max = -3
 
 Console.Clear();
+int a = 2;
+int b = 10;
+
+int max = a;
+int min = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (a < min ) min = a;
+if (b < min ) min = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
